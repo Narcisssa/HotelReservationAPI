ae8e5cd241fb8f7be77b644e5cf790a11855c339
mikita <mik.bchrd@gmail.com>
2024-11-10 11:06:46 -0300

    initial commit

---
+++ HotelReservationAPI/Models/Reservation.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelReservationAPI.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int RoomId { get; set; }
+        public  Room? Room { get; set; }
+        public  string? UserId { get; set; }
+        public  ApplicationUser? User { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+
+}
